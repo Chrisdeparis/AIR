e11d4ed75c4b1d8ea8d36bd6f9993d7092d52ca0
maescristro <Chrisdeparis@users.noreply.github.com>
2022-11-24 10:58:41 +0100

    Update air02.js

--- air02.js
+++ air02.js
@@ -4,56 +4,40 @@
 // 	# votre algorithme
 // 	return (tableau)
 // }
+let args = process.argv.slice(2);
 
-let arg = process.argv.slice(2)[0];
-let arg2 = process.argv.slice(2)[1];
-
-if (
-    arg === undefined ||
-    arg2 === undefined ||
-  arg.lenght === 0 ||
-  arg2.length === 0 ||
-  arg2 === "" ||
-  arg === "" 
-  
-) {
-  console.log("error");
-  return;
-} else {
-  let str = arg.toString();
-  let str2 = arg2.toString();
-
-  if (arg.includes(arg2)) {
-    let remove = arg2;
-    // separateur virgule
-    let reg = /\,/g;
-    let spaceStr = str.replace(reg, " ");
-
-    const ma_fonction = (str, str2) => {
-      let spaceStr = str.replace(reg, " ");
-      let resultat = [];
-      // console.log(str);
-      resultat.push(str);
-      let input = resultat.toString();
-      let arr = input.split(" ");
-
-      for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i] === arg2) {
-          // console.log(arr.indexOf(arg2));
-          let line1 = arr.slice(0, arr.indexOf(arg2)).toString();
-          let line2 = arr.slice(arr.indexOf(arg2) + 1).toString();
-          let reponse1 = line1.replace(reg, " ");
-          let reponse2 = line2.replace(reg, " ");
-          console.log(reponse1);
-          console.log(reponse2);
-          delete arr[i];
-        }
-      }
-    };
-
-    ma_fonction(spaceStr, remove);
-  } else {
-    console.log("error");
-    return;
-  }
+function my_split(sentence, separator) {
+  return sentence.split(separator);
 }
+
+
+const errorMessage = new Error('error');
+
+function areThereExactlyTwoArguments(args) {
+  return args.length == 2;
+}
+
+
+function checkArgumentForError(args) {
+  let testResult = 1;
+  if (!areThereExactlyTwoArguments(args)) {
+    //1.a if not exatly two arguments are passed
+    testResult = errorMessage;
+  }
+  return testResult;
+}
+
+// let args = process.argv.slice(2);
+// console.log(args.length);
+let checkResult = checkArgumentForError(args);
+let result = 'error';
+
+
+if (checkResult !== 1) {
+  result = checkResult.message;
+} else {
+  result = my_split(args[0], args[1]).join('\n');
+}
+
+
+console.log(result);
