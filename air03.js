69a321d8d6b128befd34efc2f90599db4ce2d3ed
maescristro <milfortchristian@gmail.com>
2022-12-17 01:02:28 +0100

    air3.js OK

--- air03.js
+++ air03.js
@@ -8,14 +8,47 @@
 // $> python exo.py “je” “teste” “des” “trucs” “ “
 // Je teste des trucs
 
-let args = process.argv.slice(2);
-console.log(args);
-let separateur = " ";
-const ma_fonction = (array_de_strings, separator) => {
-  let str = args.toString();
-  let reg = /[^a-zA-Z]+/g;
-  // enlever tous les autres caractères
-  let check = str.replace(reg, separateur);
-  console.log(check);
-};
-ma_fonction(args, separateur);
+function ma_fonction(array_de_strings, separateur) {
+  // On vérifie que array_de_strings est bien un tableau
+  if (!Array.isArray(array_de_strings)) {
+    console.log("Error: array_de_strings n'est pas un tableau");
+    return;
+  }
+
+  // On vérifie que separateur est bien une chaîne de caractères
+  if (typeof separateur !== 'string') {
+    console.log("Error: separateur n'est pas une chaîne de caractères");
+    return;
+  }
+
+  // Le reste de votre code ici...
+  // On initialise la chaîne qui contiendra le résultat final
+  let resultat = "";
+
+  // On parcourt le tableau
+  for (let i = 0; i < array_de_strings.length; i++) {
+    // On ajoute la chaîne courante au résultat
+    resultat += array_de_strings[i];
+
+    // Si ce n'est pas le dernier élément du tableau, on ajoute le séparateur
+    if (i < array_de_strings.length - 1) {
+      resultat += separateur;
+    }
+  }
+
+  // On retourne le résultat final
+  return resultat;
+}
+
+// On récupère les arguments passés au programme
+const array_de_strings = process.argv.slice(2, -1);
+const separateur = process.argv.slice(-1)[0];
+
+// On vérifie si aucun argument n'a été passé
+if (!array_de_strings.length || !separateur) {
+  console.log("Error: aucun argument n'a été passé au programme");
+  return;
+}
+
+// On appelle la fonction en utilisant les arguments récupérés
+console.log(ma_fonction(array_de_strings, separateur));
